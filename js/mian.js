f9b9c208945c8b47e34e4339ed6c729c2b741864
theMosi <mostafawshe@gmail.com>
2023-04-03 06:48:30 +0330

    side-bar-items

--- js/mian.js
+++ js/mian.js
@@ -9,6 +9,11 @@ const mainElem = $.querySelector('.main');
 const burgerMenu = $.querySelector('.burger-menu');
 const closeMenu = $.querySelector('.close-menu');
 
+const dropElems = $.querySelectorAll('.drop-item');
+const subMenusElem = $.querySelectorAll('.submenu');
+
+console.log(dropElems);
+
 if(window.innerWidth <= 992){
     open = true;
     sidebarElem.classList.add('inactive');
@@ -31,4 +36,10 @@ let toggleFunc = () => {
 
 burgerMenu.addEventListener('click',toggleFunc);
 closeMenu.addEventListener('click',toggleFunc);
+
+dropElems.forEach((dropElem) => {
+    dropElem.addEventListener('click', () => {
+       dropElem.nextElementSibling.classList.toggle('inactive')
+    })
+})
 
